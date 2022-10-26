5f35eeffc5a9ea62f672a9c852bf79f097c6234a
Gregory Maldonado <gmaldona@oswego.edu>
2022-10-26 15:17:59 -0400

    separated currency struct

---
+++ api/structs/Currency.go
@@ -0,0 +1,18 @@
+package structs
+
+type Currency_t struct {
+	Id                string `json:"id"`
+	Rank              string `json:"rank"`
+	Symbol            string `json:"symbol"`
+	Name              string `json:"name"`
+	Supply            string `json:"supply"`
+	MaxSupply         string `json:"maxSupply"`
+	MarketCapUsd      string `json:"marketCapUsd"`
+	VolumeUsd24Hr     string `json:"volumeUsd24Hr"`
+	PriceUsd          string `json:"priceUsd"`
+	ChangePercent24Hr string `json:"changePercent24Hr"`
+	Vwap24Hr          string `json:"vwap24Hr"`
+	Explorer          string `json:"explorer"`
+}
+
+const DB_TABLE_NAME = "Maldonado-CryptoBro"
