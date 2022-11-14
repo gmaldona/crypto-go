49c5cb819f0c48315bc241f5b625fbeeff6f0370
Gregory Maldonado <gmaldona@oswego.edu>
2022-11-14 08:26:08 -0500

    file dir changes

--- api/scripts/build_image.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-if [ -f crypto.go ]
-then
-  printf "Please run script in the working directory of api/"
-fi
-
-docker build -t crypto-app .
--- api/scripts/run_container.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-docker stop crypto-app
-docker rm crypto-app
-docker run -it --name crypto-app crypto-app
