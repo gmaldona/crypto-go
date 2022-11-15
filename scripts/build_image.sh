f0f63a87e5f699b6bb0f7bbe71b63b100a041a33
Gregory Maldonado <gmaldona@oswego.edu>
2022-11-15 01:53:53 -0500

    image build script for local and production

--- scripts/build_image.sh
+++ scripts/build_image.sh
@@ -1,8 +1,33 @@
 #! /bin/bash
 
-if [ -f crypto.go ]
+if [ ! -f crypto.go ]
 then
-  printf "Please run script in the working directory of api/"
+  printf "Please run script in the project root directory."
+  exit 1
 fi
 
-docker build -t crypto-app .
+IMAGE_NAME="gmaldona-poller" # image name
+
+while getopts 'lph' OPTION; do
+  case "$OPTION" in
+    l)
+      printf "Building image for local machine.\n"
+      docker image rm "$IMAGE_NAME"-local
+      docker build -t "$IMAGE_NAME"-local .
+      ;;
+    p)
+      printf "Building image for AWS linux machine.\n"
+      docker image rm "$IMAGE_NAME"
+      docker buildx build --platform linux/amd64 -t "$IMAGE_NAME" .
+      ;;
+    h)
+      printf "\055l\t Build a docker image for local machine.\n"
+      printf "\055p\t Build a docker image for AWS linux machine.\n"
+      ;;
+    ?)
+      printf "Building image for AWS linux machine.\n"
+      docker image rm "$IMAGE_NAME"
+      docker buildx build --platform linux/amd64 -t "$IMAGE_NAME" .
+  ;;
+esac
+done
